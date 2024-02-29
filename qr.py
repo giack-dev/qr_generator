8c52538e0fc3bd8707b5afc27e464d977e9fe3f1
giack-dev <github.giack@gmail.com>
2024-02-29 22:12:58 +0100

    qr.py: changes related to exception handling

--- qr.py
+++ qr.py
@@ -1,7 +1,6 @@
 import qrcode
 
-def create_qr_code(input_data: str) -> bool:
-    filename = "generated_qr.jpg"
+def create_qr_code(input_data: str, filepath: str) -> None:
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -14,9 +13,7 @@ def create_qr_code(input_data: str) -> bool:
     img = qr.make_image(fill_color="black", back_color="white")
     img = img.resize((256, 256))
     try:
-        img.save(filename)
-        print(f"Saved image as: {filename}")
-        return True
-    except:
-        print(f"There was an error creating image: {filename}")
-        return False
+        img.save(filepath)
+        print(f"Saved image as: {filepath}")
+    except Exception as e:
+        print(f"There was an error creating image: {e}")
