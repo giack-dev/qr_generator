c2a71571be77e9fdca4642e7076838292bbaa57e
giack-dev <github.giack@gmail.com>
2024-02-29 19:49:06 +0100

    qr.py: added error checking to qr creation

--- qr.py
+++ qr.py
@@ -1,6 +1,6 @@
 import qrcode
 
-def create_qr_code(input_data: str) -> None:
+def create_qr_code(input_data: str) -> bool:
     file_name = "generated_qr.jpg"
     qr = qrcode.QRCode(
         version=1,
@@ -13,7 +13,10 @@ def create_qr_code(input_data: str) -> None:
 
     img = qr.make_image(fill_color="black", back_color="white")
     img = img.resize((256, 256))
-    img.save(file_name)
-    print(f"Saved image as: {file_name}")
-
-
+    try:
+        img.save(file_name)
+        print(f"Saved image as: {file_name}")
+        return True
+    except:
+        print(f"There was an error creating image: {file_name}")
+        return False
