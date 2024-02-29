dea56ff01b5038fdac32179b2e1a1d771b7537fc
giack-dev <github.giack@gmail.com>
2024-02-29 22:14:00 +0100

    app.py: added integration with gradio and logic to handle the file saving in a specific folder

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
 import gradio as gr
 import validators as val
+import random
+from typing import Tuple
+import os
 import qr
 
 
 def validate_input_url(url: str) -> bool:
-   val.url(url)
-
-def main():
-    inputURL: gr.Textbox = gr.Textbox(label="URL", value="Enter a URL(e.g. https://www.google.com/)", lines="1", max_lines="10", show_label=True)
+    return val.url(url)
     
-    app: gr.Interface = gr.Interface(
-        fn=validate_input_url,
-        inputs=[inputURL],
-        outputs=["text"],
+def generate_qr(url: str, file: str = './generated_qr_codes') -> Tuple[str, str]:
+    if validate_input_url(url):
+        random_image_number = random.randint(1, 10000)
+        filename = f"generated_qr_{random_image_number}.jpg"
+        filepath = os.path.join(file, filename)
+        qr.create_qr_code(url, filepath)
+        return "QR Code generated successfully.", filepath
+    else:
+        return "Invalid URL. Cannot generate QR Code.", ""
+
+
+def main() -> None:
+    input_url = gr.Textbox(label="URL", placeholder="Enter a Full URL(e.g. https://www.google.com/)", interactive=True, lines="1", max_lines="10", show_label=True)
+    output_text = gr.Textbox(label="Message", placeholder="", interactive=False, lines="1", max_lines="10", show_label=True)
+    generated_qr = gr.Image(type="filepath", height=256, width=256, show_download_button=True, show_share_button=True)
+
+    app = gr.Interface(
+        title="QR Code Generator",
+        fn=generate_qr,
+        inputs=input_url,
+        outputs=[output_text, generated_qr],
     )
     
     app.launch()
