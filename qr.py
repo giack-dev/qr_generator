c4d698c7913bedf9b9deb4d373ab8f5cb8cd16af
giack-dev <github.giack@gmail.com>
2024-03-03 17:32:22 +0100

    converted all code files to use single quotes for stirngs

--- qr.py
+++ qr.py
@@ -10,10 +10,10 @@ def create_qr_code(input_data: str, filepath: str) -> None:
     qr.add_data(input_data)
     qr.make(fit=True)
 
-    img = qr.make_image(fill_color="black", back_color="white")
+    img = qr.make_image(fill_color='black', back_color='white')
     img = img.resize((256, 256))
     try:
         img.save(filepath)
-        print(f"Saved image as: {filepath}")
+        print(f'Saved image as: {filepath}')
     except Exception as e:
-        print(f"There was an error creating image: {e}")
+        print(f'There was an error creating image: {e}')
